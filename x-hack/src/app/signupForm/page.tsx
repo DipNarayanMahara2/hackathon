3fce1b96b767e4c72753cca1695434a6b1d354ce
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2025-01-25 12:26:47 +0545

    full login page

---
+++ x-hack/src/app/signupForm/page.tsx
@@ -0,0 +1,10 @@
+// pages/login.tsx (Login page)
+"use client";
+
+import SignupForm from "@/components/signupForm";
+
+const signupForm = () => {
+  return <SignupForm />;
+};
+
+export default signupForm;
